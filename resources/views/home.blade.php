b27d7cc29ccc9d759b739235b621e74402dbd69b
Giedre Klimaite <giedre.klima@gmail.com>
2021-04-28 22:13:53 +0300

    php artisan make:model Profile

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,19 +3,22 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-3 p-5">
+        <div class="col-3 p-5 d-flex flex-wrap">
             <img src="https://images.unsplash.com/photo-1496459169807-866e74594fa8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80" style="height:5rem;" class="rounded-circle">
         </div>
         <div class="col-9 pt-5">
-            <div><h1>Title</h1></div>
+            <div class="d-flex justify-content-between align-items-baseline"> 
+                <h1>{{ $user->name }}</h1>
+                <a href="#">Add New Post</a>
+            </div>
             <div class="d-flex">
                 <div class="pr-5"><strong>153</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
-            <div class="pt-4 font-weight-bold">Lorem ipsum</div>
-            <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.</div>
-            <div><a href="#">www.loremipsum.com</a></div>
+            <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+            <div>{{ $user->profile->description }}</div>
+            <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
     </div>
 
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+ 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/profile/{user}', [App\Http\Controllers\ProfilesController::class, 'index'])->name('profile.show');
