3c7d7e50101ddf4919a003f08c23c49bee0eef3a
Giedre Klimaite <giedre.klima@gmail.com>
2021-04-29 19:55:22 +0300

    post text, img validation, less hardcoded info

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -9,10 +9,10 @@
         <div class="col-9 pt-5">
             <div class="d-flex justify-content-between align-items-baseline"> 
                 <h1>{{ $user->name }}</h1>
-                <a href="#">Add New Post</a>
+                <a href="/p/create">Add New Post</a>
             </div>
             <div class="d-flex">
-                <div class="pr-5"><strong>153</strong> posts</div>
+                <div class="pr-5"><strong>{{ $user->posts->count() }}</strong> posts</div>
                 <div class="pr-5"><strong>23k</strong> followers</div>
                 <div class="pr-5"><strong>212</strong> following</div>
             </div>
@@ -20,18 +20,14 @@
             <div>{{ $user->profile->description }}</div>
             <div><a href="#">{{ $user->profile->url }}</a></div>
         </div>
-    </div>
+    </div> 
 
 <div class="row pt-5">
-    <div class="col-4">
-        <img src="https://images.unsplash.com/photo-1587279063133-e65e2d8b849f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80" class="w-100">
-    </div>
-    <div class="col-4">
-        <img src="https://images.unsplash.com/photo-1482784160316-6eb046863ece?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" class="w-100">
-    </div>
-    <div class="col-4">
-        <img src="https://images.unsplash.com/photo-1563547257011-054b1054e185?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1051&q=80" class="w-100">
-    </div>
+   @foreach($user->posts as $post)
+       <div class="col-4 pb-4">
+            <img src="/storage/{{ $post->image }}" class="w-100">
+        </div>
+    @endforeach
 </div>
 
 </div>
